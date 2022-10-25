e6b720049d7d106db37a151953995e1d6ac1cb35
mlagasse-cstj <2068410@cstj.qc.ca>
2022-10-25 15:45:52 -0400

    Debut du projet.

---
+++ src/views/pages/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="py-4 container-fluid">
+    <h1>Andromia</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from '../../components/HelloWorld.vue';
+</script>
+
+<style lang="scss" scoped></style>
