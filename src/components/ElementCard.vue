525421470e5310200efc643f35f85c5acdee9e1a
mlagasse-cstj <2068410@cstj.qc.ca>
2022-11-10 16:30:09 -0500

    Les cards d'un element sont maintenant des components.

---
+++ src/components/ElementCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <img style="margin: auto 0px;"
+            v-bind:src="'https://assets.andromia.science/elements/' + element.element + '.png'"
+            v-bind:title="element.element" />
+        <h2>{{element.element}}</h2>
+        <h3>x {{element.quantity}}</h3>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    element: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
